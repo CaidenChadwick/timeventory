ba9f72e302d45e72021084500e6888e57a90d297
Eric-Chrisman <ericchrisman01@gmail.com>
2024-09-25 15:16:10 -0500

    created Following Table

--- Models/orgModel.ts
+++ Models/orgModel.ts
@@ -4,7 +4,10 @@ import prisma from '@/db';
 import { getUserId } from '@/Models/SessionModel';
 import { Status } from '@/types/databaseUtilityTypes';
 
-// 
+// creates an orgzation
+// requires a user to be owner
+// requires a unique name
+// the user must be logged in
 export async function createOrg(name: string, session: string): Promise<Status>{
     const status: Status = {
         success: true,
@@ -33,6 +36,7 @@ export async function createOrg(name: string, session: string): Promise<Status>{
             //todo: if error cause name not unique, send special error messesage
         }
     }
-
     return status;
 }
+
+//todo when event started;export async function findEventsHostedByOrg
---
+++ app/createOrg/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const testName = "brugerTank"
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Timeventory</h1>
+      <p>Welcome to the</p>
+      <p className = 'orange'>ORG PAGE</p>
+      <button>createOrg</button>
+    </div>
+  );
+};
+
+export default HomePage;
