d48233d2010c8a552c5166c1d3cd58f8d027d153
CaidenChadwick <caidenchadwick03@gmail.com>
2024-10-01 21:07:31 -0500

    Updated navbar using wrapper and modal functions

---
+++ Components/Navbar/NavbarClient.tsx
@@ -0,0 +1,69 @@
+'use client'
+import React from 'react';
+import { useState } from 'react';
+import { MouseEvent } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { logoutAction } from './actions';
+import LoginModal from '../Modals/LoginModal/LoginModal';
+import RegistrationModal from '../Modals/RegistrationModal/RegistrationModal';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+
+export default function NavbarClient({ loggedIn }: { loggedIn: boolean }) {
+
+  const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
+  const [showRegisterModal, setShowRegisterModal] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const toggleLoginModal = () => {
+    setShowLoginModal(!showLoginModal);
+  }
+
+  const toggleRegisterModal = () => {
+    setShowRegisterModal(!showRegisterModal);
+  }
+
+  // Handle Logout click
+  const handleLogOut = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const message = await logoutAction();
+    setErrorMessage(message);
+
+  }
+
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="/">
+          Logo
+        </Navbar.Brand>
+      </Container>
+      <LoginModal
+        showLoginModal={showLoginModal}
+        toggleLoginModal={toggleLoginModal}
+      />
+      <RegistrationModal
+        showRegisterModal={showRegisterModal}
+        toggleRegisterModal={toggleRegisterModal}
+      />
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        {!loggedIn &&
+          <Nav>
+            <Nav.Link onClick={toggleLoginModal}>Log In</Nav.Link>
+            <Nav.Link onClick={toggleRegisterModal}>Register</Nav.Link>
+          </Nav>
+        }
+
+        {loggedIn &&
+          <Nav>
+            <Nav.Link onClick={handleLogOut}>Log Out</Nav.Link>
+          </Nav>
+        }
+      </Navbar.Collapse>
+    </Navbar>
+  )
+};
