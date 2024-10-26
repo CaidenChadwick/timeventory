035e119c89ec4768ef88c6a348d70691143ff11f
CaidenChadwick <caidenchadwick03@gmail.com>
2024-10-25 20:14:38 -0500

    fixed modal errors upon reattempting login

--- Components/Modals/LoginModal/LoginModal.tsx
+++ Components/Modals/LoginModal/LoginModal.tsx
@@ -14,47 +14,51 @@ export default function LoginModal({ showLoginModal, toggleLoginModal }: { showL
     // Handle the form submission
     const handleSumbit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        
+
         const loginData: LoginData = {
             username: usernameState,
             password: passwordState
         };
 
         setError(!(await loginAction(loginData)));
-        
+
         if (!error) {
             toggleLoginModal();
         }
     }
 
+    const handleModalOpen = () => {
+        setError(false);
+    }
+
     return (
-        <Modal show={showLoginModal} onHide={toggleLoginModal} animation={false}>
+        <Modal show={showLoginModal} onShow={handleModalOpen} onHide={toggleLoginModal} animation={false}>
             <Modal.Header closeButton>
                 <Modal.Title>Login</Modal.Title>
             </Modal.Header>
             <Form noValidate validated={false} onSubmit={handleSumbit}>
-            <Modal.Body>
-                
+                <Modal.Body>
+
                     <Form.Group className="mb-3">
                         <Form.Label><b>Username</b></Form.Label>
-                        <Form.Control 
-                            placeholder="Username" 
+                        <Form.Control
+                            placeholder="Username"
                             required
                             isInvalid={error}
-                            onChange={(e) => setUsernameState(e.target.value)} 
+                            onChange={(e) => setUsernameState(e.target.value)}
                         />
-                    <Form.Control.Feedback type="invalid">
-                        Incorrect Username and/or Password.
-                    </Form.Control.Feedback>
+                        <Form.Control.Feedback type="invalid">
+                            Incorrect Username and/or Password.
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label><b>Password</b></Form.Label>
-                        <Form.Control 
-                            type="password" 
+                        <Form.Control
+                            type="password"
                             placeholder="Password"
                             isInvalid={error}
-                            required 
-                            onChange={(e) => setPasswordState((e.target.value))} 
+                            required
+                            onChange={(e) => setPasswordState((e.target.value))}
                         />
                         <Form.Control.Feedback type="invalid">
                             Incorrect Username and/or Password.
